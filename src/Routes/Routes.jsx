01b74eae3172da6286fd82a67b17aa204163bd12
Anuwar-Hosain <anuwarhossn@gmail.com>
2023-05-19 13:13:03 +0600

    register section added done

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../page/Home/Home/Home";
 import Login from "../page/Login/Login";
+import Register from "../page/Register/Register";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: "login",
         element: <Login></Login>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
       },
     ],
   },
--- src/page/Login/Login.jsx
+++ src/page/Login/Login.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { BsGoogle } from "react-icons/bs";
 
 const Login = () => {
   return (
     <section className="size">
-      <div className="hero min-h-[90vh]">
+      <div className="hero min-h-[90vh] font">
         <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
             <div className="form-control">
@@ -26,14 +27,21 @@ const Login = () => {
                 placeholder="password"
                 className="input input-bordered"
               />
-              <label className="label">
-                <Link to="/register" className="label-text-alt link link-hover">
-                  Forgot password?
-                </Link>
-              </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <button className="btn btn-primary mainColor ">Login</button>
+            </div>
+            <div className="text-center flex flex-col items-center justify-center">
+              <p>Or Sign In with</p>
+              <div className="bg-slate-300 p-2 w-8 h-8 rounded-full text-center cursor-pointer">
+                <BsGoogle></BsGoogle>
+              </div>
+            </div>
+            <div className="">
+              Have an account?
+              <Link to="/register" className="ml-2 mainColor">
+                Sign In
+              </Link>
             </div>
           </div>
         </div>
